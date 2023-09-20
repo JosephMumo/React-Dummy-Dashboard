ed21bd0e7a4f6ecbc505b283244ae72c7b59cfed
JosephMumo <josephmumo25@gmail.com>
2023-09-20 17:04:15 +0300

    feat:header alignment

--- dummy-dashboard/src/components/Header.jsx
+++ dummy-dashboard/src/components/Header.jsx
@@ -6,8 +6,8 @@ import { SearchIcon } from '@heroicons/react/solid'
 function Header() {
   return (
     <div className='p-2 flex flex-col space-y-2 md:flex-row justify-between w-full' id='home'>
-        <h2 className='font-bold text-2xl text-start text-gray-300 mt-5'>Dashboard</h2>
-        <div className='mt-5 md:right-3'>
+        <h2 className='font-bold text-2xl text-start text-gray-300 mt-5 ml-2'>Dashboard</h2>
+        <div className='mt-5 md:right-3 ml-2'>
             <TextInput icon={SearchIcon} placeholder='Search...' />
         </div>
     </div>
